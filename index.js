bdc71640ebf0f2d7546840345074320eab498b49
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-01-31 16:40:00 -0500

    commit tree 3bb1db311bd656b1f742a0477dddcaef596d5111

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greeting;
+  if (feet<=400) {
+    greeting = 'This one is on me!';
+  } else if (feet>2500){
+    greeting = 'No can do.';
+  } else if (feet>2000){
+    greeting = 'I will gladly take your thirty bucks.';
+  } 
+  return greeting;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city==='NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let goodBye;
+  switch(tip){
+    case 'generous':
+    goodBye = 'Thank you so much.';
+    break;
+    case 'not as generous':
+    goodBye = 'Thank you.';
+    break;
+    default:
+    goodBye = 'Bye.';
+    break;
+  }
+
+  return goodBye;
 }
